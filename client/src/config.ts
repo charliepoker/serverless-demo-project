5ba12e33c3ec411451478d5103158f0aba4a8504
charliepoker <iheanachocharlie@gmail.com>
2022-10-04 17:17:29 +0100

    initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-f8mroul0.us.auth0.com',
+  clientId: 'VsMKmb2wLYNcNYwU2V2ly4b8gOWnqMdF',
+  callbackUrl: 'http://localhost:3000/callback'
+}

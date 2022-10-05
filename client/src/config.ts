c5fe626cc8a5a32734aad991709475caf4eb89b1
charliepoker <iheanachocharlie@gmail.com>
2022-10-05 01:02:02 +0100

    complete

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
-const apiId = '';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = 'o5bcocjiqf'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
 
 export const authConfig = {
   domain: 'dev-f8mroul0.us.auth0.com',
